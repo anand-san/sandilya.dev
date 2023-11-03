0bc3d06233f83c3cd1d968c015e969050010ebf5
Anand Sandilya <anand.sandilya@quantilope.com>
2023-11-03 09:20:24 +0100

    feat(structure): move project inside src folder

--- src/components/footer-contact-card/footer-contact-card.tsx
+++ src/components/footer-contact-card/footer-contact-card.tsx
@@ -3,11 +3,11 @@ import {
   Tooltip,
   TooltipTrigger,
   TooltipContent,
-} from "@/components/ui/tooltip";
-import { Instagram, Linkedin, Twitter, Github } from "lucide-react";
-import getConfig from "next/config";
-import Link from "next/link";
-import React from "react";
+} from '@/components/ui/tooltip';
+import { Instagram, Linkedin, Twitter, Github } from 'lucide-react';
+import getConfig from 'next/config';
+import Link from 'next/link';
+import React from 'react';
 
 export default function FooterContactCard() {
   const {
@@ -15,72 +15,72 @@ export default function FooterContactCard() {
   } = getConfig();
 
   return (
-    <section className="md:h-screen pt-8 md:pt-0 grid md:snap-start md:snap-always items-center">
+    <section className='grid items-center pt-8 md:h-screen md:snap-start md:snap-always md:pt-0'>
       <div
-        className={`flex flex-col m-4 p-8 justify-between h-56 sm:h-72 text-center`}
+        className={`m-4 flex h-56 flex-col justify-between p-8 text-center sm:h-72`}
       >
-        <h1 className="text-2xl md:text-3xl">Want to work together?</h1>
-        <p className="text-s font-extralight">
+        <h1 className='text-2xl md:text-3xl'>Want to work together?</h1>
+        <p className='text-s font-extralight'>
           Feel free to reach out for collaborations or just a friendly hello
         </p>
-        <span className="text-s font-light">hello@anandks.com</span>
-        <div className="flex mt-12 justify-center">
+        <span className='text-s font-light'>hello@anandks.com</span>
+        <div className='mt-12 flex justify-center'>
           <Tooltip>
             <TooltipTrigger>
-              <Link href={SOCIAL_URLS.INSTAGRAM} target="_blank">
+              <Link href={SOCIAL_URLS.INSTAGRAM} target='_blank'>
                 <Instagram
                   strokeWidth={1.25}
                   size={36}
-                  className="ml-4 text-[#d62976] hover:fill-[#f7d6bc] cursor-pointer"
+                  className='ml-4 cursor-pointer text-[#d62976] hover:fill-[#f7d6bc]'
                 />
               </Link>
             </TooltipTrigger>
-            <TooltipContent side="bottom">
+            <TooltipContent side='bottom'>
               <p>Instagram</p>
             </TooltipContent>
           </Tooltip>
 
           <Tooltip>
             <TooltipTrigger>
-              <Link href={SOCIAL_URLS.LINKEDIN} target="_blank">
+              <Link href={SOCIAL_URLS.LINKEDIN} target='_blank'>
                 <Linkedin
                   strokeWidth={1.25}
                   size={36}
-                  className="ml-4 text-[#0072b1] hover:fill-[#0072b1] cursor-pointer"
+                  className='ml-4 cursor-pointer text-[#0072b1] hover:fill-[#0072b1]'
                 />
               </Link>
             </TooltipTrigger>
-            <TooltipContent side="bottom">
+            <TooltipContent side='bottom'>
               <p>Linkedin</p>
             </TooltipContent>
           </Tooltip>
 
           <Tooltip>
             <TooltipTrigger>
-              <Link href={SOCIAL_URLS.TWITTER} target="_blank">
+              <Link href={SOCIAL_URLS.TWITTER} target='_blank'>
                 <Twitter
                   strokeWidth={1.25}
                   size={36}
-                  className="ml-4 text-[#1DA1F2] hover:fill-[#1DA1F2] cursor-pointer"
+                  className='ml-4 cursor-pointer text-[#1DA1F2] hover:fill-[#1DA1F2]'
                 />
               </Link>
             </TooltipTrigger>
-            <TooltipContent side="bottom">
+            <TooltipContent side='bottom'>
               <p>Twitter/ X</p>
             </TooltipContent>
           </Tooltip>
 
           <Tooltip>
             <TooltipTrigger>
-              <Link href={SOCIAL_URLS.GITHUB} target="_blank">
+              <Link href={SOCIAL_URLS.GITHUB} target='_blank'>
                 <Github
                   strokeWidth={1.25}
                   size={36}
-                  className="ml-4 text-[#171515] hover:fill-[#171515] dark:text-[#fff] dark:hover:fill-[#fff] cursor-pointer"
+                  className='ml-4 cursor-pointer text-[#171515] hover:fill-[#171515] dark:text-[#fff] dark:hover:fill-[#fff]'
                 />
               </Link>
             </TooltipTrigger>
-            <TooltipContent side="bottom">
+            <TooltipContent side='bottom'>
               <p>Github</p>
             </TooltipContent>
           </Tooltip>
---
+++ src/components/testomonials-card.tsx
@@ -0,0 +1,129 @@
+'use client';
+import Image, { StaticImageData } from 'next/image';
+import React, { useState } from 'react';
+import SauravImage from '../../public/images/testimonials/saurav.png';
+import RizwanImage from '../../public/images/testimonials/rizwan.png';
+import HarshitImage from '../../public/images/testimonials/harshit.png';
+import { cn } from '@/lib/utils';
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from '@/components/ui/tooltip';
+import { Linkedin } from 'lucide-react';
+import Link from 'next/link';
+
+export default function TestomonialsCard() {
+  const [activeTestimonialIndex, setActiveTestimonialIndex] =
+    useState<number>(1);
+
+  return (
+    <section className='flex items-center justify-center pt-16 md:h-screen md:snap-start md:snap-always md:pt-0'>
+      <div className='h-[70vh] w-[80vw]'>
+        <h1 className='mb-16 text-center text-3xl uppercase tracking-wider md:mb-24'>
+          What people say
+        </h1>
+        <div className='grid grid-cols-2 justify-around gap-12'>
+          <div>
+            {TESTIMONIALS.map(({ order, name, image, designation }) => (
+              <div
+                key={order}
+                className={cn(
+                  'mb-6 flex cursor-pointer rounded-xl border border-transparent px-20  py-8 hover:border-neutral-200 dark:hover:border-neutral-700',
+                  order === activeTestimonialIndex &&
+                    'border-neutral-200 dark:border-neutral-700'
+                )}
+                onClick={() => setActiveTestimonialIndex(order)}
+              >
+                <Image
+                  src={image}
+                  width={56}
+                  height={56}
+                  alt={`feedback${name}`}
+                  className='mr-4 rounded-full'
+                />
+                <div className='flex flex-col items-baseline'>
+                  <span>
+                    {name}
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <Link
+                          href={
+                            'https://www.linkedin.com/in/syed-rizwan-ali-344b151b/'
+                          }
+                          target='_blank'
+                        >
+                          <Linkedin
+                            strokeWidth={1.25}
+                            size={14}
+                            className='ml-4 cursor-pointer text-[#0072b1] hover:fill-[#0072b1]'
+                          />
+                        </Link>
+                      </TooltipTrigger>
+                      <TooltipContent side='top'>
+                        <p>Linkedin</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </span>
+                  <span className='font-semilight text-sm text-zinc-400'>
+                    {designation}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='flex flex-col'>
+            <h1 className='mb-12 text-xl font-bold tracking-wider'>
+              {TESTIMONIALS[activeTestimonialIndex - 1].shortMessage}{' '}
+            </h1>
+            <p className='leading-6 tracking-wide'>
+              {TESTIMONIALS[activeTestimonialIndex - 1].message}
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className='m-auto mt-16 w-[20vw] dark:bg-white md:hidden' />
+    </section>
+  );
+}
+
+interface TestomonialProps {
+  order: number;
+  name: string;
+  image: StaticImageData;
+  designation: string;
+  shortMessage: string;
+  message: string;
+}
+const TESTIMONIALS: TestomonialProps[] = [
+  {
+    name: 'Saurav Mishra',
+    image: SauravImage,
+    designation: 'Senior Engineering Manager',
+    shortMessage: 'A reliable and highly capable professional',
+    message:
+      'Anand is passionate about writing quality code and solving problems through the development of creative and innovative software. His skills and knowledge are unparalleled. He had the most extensive and diverse knowledge of software in the team. A reliable and highly capable professional.',
+    order: 1,
+  },
+  {
+    name: 'Harshit Shrivastava',
+    image: HarshitImage,
+    designation: 'Senior Developer',
+    shortMessage: 'Excellent in writing clean and organised code',
+    message:
+      'Anand is keenly devoted to work. He demonstrated from early in tenure that he is excellent in writing clean and organised code. He is an Exceptional software engineer, very easy going and enjoyable to be around.',
+    order: 2,
+  },
+  {
+    name: 'Syed Rizwan Ali Mishra',
+    image: RizwanImage,
+    designation: 'Team Lead',
+    shortMessage:
+      'Excellent job in managing and improving our systems consistently',
+    message:
+      'Anand has always stayed up to date on software developments and finding new solutions. He has also done an excellent job in managing and improving our systems consistently. Along with these, He bought energy and dependability that has made him crucial to the team.',
+
+    order: 3,
+  },
+];
