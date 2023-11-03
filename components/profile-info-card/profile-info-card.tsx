e1051728d5aa69a4b374b455340222d792b986b4
Anand Sandilya <anand.sandilya@quantilope.com>
2023-11-03 08:42:56 +0100

    feat(prettier): configure tailwind prettier plugin

--- components/profile-info-card/profile-info-card.tsx
+++ components/profile-info-card/profile-info-card.tsx
@@ -1,15 +1,15 @@
-"use client";
-import { Instagram, Linkedin, Twitter, Github } from "lucide-react";
-import getConfig from "next/config";
-import Link from "next/link";
+'use client';
+import { Instagram, Linkedin, Twitter, Github } from 'lucide-react';
+import getConfig from 'next/config';
+import Link from 'next/link';
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { useRef, useLayoutEffect } from "react";
-import { gsap } from "gsap";
+} from '@/components/ui/tooltip';
+import { useRef, useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
 
 export const ProfileInfoCard = () => {
   const profileSection = useRef(null);
@@ -17,16 +17,16 @@ export const ProfileInfoCard = () => {
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
       // use scoped selectors
-      gsap.from(".profile-image", {
+      gsap.from('.profile-image', {
         x: 500,
         duration: 1.5,
-        ease: "power1.out(1)",
+        ease: 'power1.out(1)',
       });
 
-      gsap.from(".profile-description", {
+      gsap.from('.profile-description', {
         x: -1000,
         duration: 1.5,
-        ease: "power1.out(1.7)",
+        ease: 'power1.out(1.7)',
       });
     }, profileSection);
 
@@ -34,96 +34,96 @@ export const ProfileInfoCard = () => {
   }, []);
 
   const SOCIAL_URLS = {
-    TWITTER: "https://twitter.com/anandsan_",
-    LINKEDIN: "https://www.linkedin.com/in/anandsandilya/",
-    INSTAGRAM: "https://www.instagram.com/anandsan_/",
-    GITHUB: "https://github.com/anand-san",
+    TWITTER: 'https://twitter.com/anandsan_',
+    LINKEDIN: 'https://www.linkedin.com/in/anandsandilya/',
+    INSTAGRAM: 'https://www.instagram.com/anandsan_/',
+    GITHUB: 'https://github.com/anand-san',
   };
 
   return (
     <section
       ref={profileSection}
-      className="profile-section lg:container mx-auto pt-4 sm:pt-8 md:h-screen grid md:grid-cols-[2fr,1fr] md:snap-start md:snap-always items-center place-items-center"
+      className='profile-section mx-auto grid place-items-center items-center pt-4 lg:container sm:pt-8 md:h-screen md:snap-start md:snap-always md:grid-cols-[2fr,1fr]'
     >
       <div
-        className={`profile-image border-solid border-neutral-300	 dark:border-neutral-700 bg-[url('/images/profile.png')] bg-left bg-cover bg-no-repeat md:order-2 border rounded-full md:rounded-xl m-4 scale-x-[-1] min-h-[200px] min-w-[200px] md:min-w-[384px] md:grid md:min-h-[384px] shadow-xl`}
+        className={`profile-image m-4 min-h-[200px]	 min-w-[200px] scale-x-[-1] rounded-full border border-solid border-neutral-300 bg-[url('/images/profile.png')] bg-cover bg-left bg-no-repeat shadow-xl dark:border-neutral-700 md:order-2 md:grid md:min-h-[384px] md:min-w-[384px] md:rounded-xl`}
       ></div>
 
       <div
-        className={`profile-description md:order:1 flex flex-col p-8 pb-0 md:m-4 justify-between min-h-[384px]`}
+        className={`profile-description md:order:1 flex min-h-[384px] flex-col justify-between p-8 pb-0 md:m-4`}
       >
-        <h1 className="font-semibold text-center md:text-left text-2xl md:text-4xl">
+        <h1 className='text-center text-2xl font-semibold md:text-left md:text-4xl'>
           Hello, I&apos;m Anand, a Software Developer With 7 years of
           experience.
         </h1>
-        <p className="font-normal text-l text-center md:text-left md:text-xl mt-2">
+        <p className='text-l mt-2 text-center font-normal md:text-left md:text-xl'>
           I care a lot about using design for positive impact. and enjoy
           creating user-centric, delightful, and human experiences.
         </p>
-        <div className="flex items-center mt-12 flex-col md:flex-row">
-          <Link href="mailto:hello@anandks.com">
-            <button className="font-semibold bg-[#1d1d1d] hover:bg-[#3c3c3c] text-[#fff] rounded-3xl px-14 py-3 md:mr-4">
+        <div className='mt-12 flex flex-col items-center md:flex-row'>
+          <Link href='mailto:hello@anandks.com'>
+            <button className='rounded-3xl bg-[#1d1d1d] px-14 py-3 font-semibold text-[#fff] hover:bg-[#3c3c3c] md:mr-4'>
               Lets Chat
             </button>
           </Link>
-          <div className="flex mt-8 md:mt-0">
+          <div className='mt-8 flex md:mt-0'>
             <TooltipProvider delayDuration={100} skipDelayDuration={100}>
               <Tooltip>
                 <TooltipTrigger>
-                  <Link href={SOCIAL_URLS.INSTAGRAM} target="_blank">
+                  <Link href={SOCIAL_URLS.INSTAGRAM} target='_blank'>
                     <Instagram
                       strokeWidth={1.25}
                       size={36}
-                      className="ml-4 text-[#d62976] hover:fill-[#f7d6bc] cursor-pointer"
+                      className='ml-4 cursor-pointer text-[#d62976] hover:fill-[#f7d6bc]'
                     />
                   </Link>
                 </TooltipTrigger>
-                <TooltipContent side="bottom">
+                <TooltipContent side='bottom'>
                   <p>Instagram</p>
                 </TooltipContent>
               </Tooltip>
 
               <Tooltip>
                 <TooltipTrigger>
-                  <Link href={SOCIAL_URLS.LINKEDIN} target="_blank">
+                  <Link href={SOCIAL_URLS.LINKEDIN} target='_blank'>
                     <Linkedin
                       strokeWidth={1.25}
                       size={36}
-                      className="ml-4 text-[#0072b1] hover:fill-[#0072b1] cursor-pointer"
+                      className='ml-4 cursor-pointer text-[#0072b1] hover:fill-[#0072b1]'
                     />
                   </Link>
                 </TooltipTrigger>
-                <TooltipContent side="bottom">
+                <TooltipContent side='bottom'>
                   <p>Linkedin</p>
                 </TooltipContent>
               </Tooltip>
 
               <Tooltip>
                 <TooltipTrigger>
-                  <Link href={SOCIAL_URLS.TWITTER} target="_blank">
+                  <Link href={SOCIAL_URLS.TWITTER} target='_blank'>
                     <Twitter
                       strokeWidth={1.25}
                       size={36}
-                      className="ml-4 text-[#1DA1F2] hover:fill-[#1DA1F2] cursor-pointer"
+                      className='ml-4 cursor-pointer text-[#1DA1F2] hover:fill-[#1DA1F2]'
                     />
                   </Link>
                 </TooltipTrigger>
-                <TooltipContent side="bottom">
+                <TooltipContent side='bottom'>
                   <p>Twitter/ X</p>
                 </TooltipContent>
               </Tooltip>
 
               <Tooltip>
                 <TooltipTrigger>
-                  <Link href={SOCIAL_URLS.GITHUB} target="_blank">
+                  <Link href={SOCIAL_URLS.GITHUB} target='_blank'>
                     <Github
                       strokeWidth={1.25}
                       size={36}
-                      className="ml-4 text-[#171515] hover:fill-[#171515] dark:text-[#fff] dark:hover:fill-[#fff] cursor-pointer"
+                      className='ml-4 cursor-pointer text-[#171515] hover:fill-[#171515] dark:text-[#fff] dark:hover:fill-[#fff]'
                     />
                   </Link>
                 </TooltipTrigger>
-                <TooltipContent side="bottom">
+                <TooltipContent side='bottom'>
                   <p>Github</p>
                 </TooltipContent>
               </Tooltip>
@@ -131,7 +131,7 @@ export const ProfileInfoCard = () => {
           </div>
         </div>
       </div>
-      <hr className="dark:bg-white m-auto w-[20vw] mt-16 md:hidden" />
+      <hr className='m-auto mt-16 w-[20vw] dark:bg-white md:hidden' />
     </section>
   );
 };
--- components/project-info-card/project-info-card.tsx
+++ components/project-info-card/project-info-card.tsx
@@ -1,38 +1,38 @@
-import { ChevronRightCircle } from "lucide-react";
-import Link from "next/link";
+import { ChevronRightCircle } from 'lucide-react';
+import Link from 'next/link';
 
 export default function ProjectInfoCard() {
   return (
-    <section className="md:h-screen mt-6 md:mt-0 md:snap-center md:snap-always grid sm:gap-3 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 items-center mx-6">
+    <section className='mx-6 mt-6 grid items-center sm:grid-cols-2 sm:gap-3 md:mt-0 md:h-screen md:snap-center md:snap-always md:grid-cols-3 xl:grid-cols-4'>
       {/* <h1 className="text-center text-3xl my-8 uppercase tracking-wider">
         Projects
       </h1> */}
       {PROJECTS.sort(sortByOrder).map(
         ({ order, title, description, url, imagePath }) => (
-          <Link key={order} href={url} target="_blank">
+          <Link key={order} href={url} target='_blank'>
             <div
-              className={`transition-all border rounded-2xl mt-8  sm:m-y-2 h-64 md:h-[320px] ${imagePath} bg-no-repeat bg-cover bg-center cursor-pointer bg-origin-padding shadow-md`}
+              className={`sm:m-y-2 mt-8 h-64 rounded-2xl  border transition-all md:h-[320px] ${imagePath} cursor-pointer bg-cover bg-center bg-no-repeat bg-origin-padding hover:shadow-[inset_0_0_0_2000px_rgba(0,0,0,0.2)] dark:hover:shadow-[inset_0_0_0_2000px_rgba(255,255,255,0.5)]`}
             >
-              <div className="p-4 rounded-2xl rounded-b-none flex justify-between items-center">
-                <span className="whitespace-nowrap overflow-hidden text-ellipsis max-w-sm">
-                  <h1 className="text-2xl font-semibold text-zinc-800 dark:text-zinc-200">
+              <div className='flex items-center justify-between rounded-2xl rounded-b-none p-4'>
+                <span className='max-w-sm overflow-hidden text-ellipsis whitespace-nowrap'>
+                  <h1 className='text-2xl font-semibold text-zinc-800 dark:text-zinc-200'>
                     {title}
                   </h1>
-                  <h4 className="font-light text-sm text-zinc-500 dark:text-zinc-200 dark:font-semilight w-8">
+                  <h4 className='dark:font-semilight w-8 text-sm font-light text-zinc-500 dark:text-zinc-200'>
                     {description}
                   </h4>
                 </span>
 
                 <ChevronRightCircle
                   size={28}
-                  className="transition-all hover:rotate-[-45deg]"
+                  className='transition-all hover:rotate-[-45deg]'
                 />
               </div>
             </div>
           </Link>
         )
       )}
-      <hr className="dark:bg-white m-auto w-[20vw] mt-16 md:hidden" />
+      <hr className='m-auto mt-16 w-[20vw] dark:bg-white md:hidden' />
     </section>
   );
 }
@@ -49,45 +49,45 @@ interface Project {
 const PROJECTS: Project[] = [
   {
     order: 3,
-    title: "Talab App",
-    description: "Liquor Trivia App",
-    url: "https://play.google.com/store/apps/details?id=com.talabapp&hl=de&gl=US",
+    title: 'Talab App',
+    description: 'Liquor Trivia App',
+    url: 'https://play.google.com/store/apps/details?id=com.talabapp&hl=de&gl=US',
     imagePath: "bg-[url('/images/projects/talab.png')]",
   },
   {
     order: 4,
-    title: "Bluprint",
+    title: 'Bluprint',
     description:
-      "Streamlining workforce management and empowering the business to efficiently manage employee data, contracts, and compliance in one centralized platform ",
-    url: "https://ps.leadsquaredapps.com",
+      'Streamlining workforce management and empowering the business to efficiently manage employee data, contracts, and compliance in one centralized platform ',
+    url: 'https://ps.leadsquaredapps.com',
     imagePath: "bg-[url('/images/projects/bluprint.png')]",
   },
   {
     order: 1,
-    title: "Odd Beans Coffee",
+    title: 'Odd Beans Coffee',
     description: "Experiience the real taste of 'Good Coffee'",
-    url: "https://www.oddbeanscoffee.com/",
+    url: 'https://www.oddbeanscoffee.com/',
     imagePath: "bg-[url('/images/projects/oddbeanscoffee.png')]",
   },
   {
     order: 5,
-    title: "Quantilope GmbH",
-    description: "",
-    url: "https://www.quantilope.com/",
+    title: 'Quantilope GmbH',
+    description: '',
+    url: 'https://www.quantilope.com/',
     imagePath: "bg-[url('/images/projects/quantilope.webp')]",
   },
   {
     order: 6,
-    title: "Mieterengel GmbH",
-    description: "",
-    url: "https://mieterengel.de/",
+    title: 'Mieterengel GmbH',
+    description: '',
+    url: 'https://mieterengel.de/',
     imagePath: "bg-[url('/images/projects/mieterengel.jpg')]",
   },
   {
     order: 7,
-    title: "Leadsquared",
-    description: "",
-    url: "https://leadsquared.com/",
+    title: 'Leadsquared',
+    description: '',
+    url: 'https://leadsquared.com/',
     imagePath: "bg-[url('/images/projects/leadsquared.png')]",
   },
 ];
