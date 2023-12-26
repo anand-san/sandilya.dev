0124fb516df56b83b62607d7a7c8a0ae3450658a
Anand Sandilya <anand.sandilya@quantilope.com>
2023-12-26 20:50:35 +0100

    feat(main-page): remove other views for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,24 @@ import { ProfileInfoCard } from '@/components/profile-info-card';
 import ProjectInfoCard from '@/components/project-info-card';
 import FooterContactCard from '@/components/footer-contact-card';
 import TestomonialsCard from '@/components/testomonials-card';
+import './page.css';
 
 export default function Home() {
   return (
-    <>
+    <div className='base-page'>
       <ProfileInfoCard />
 
       {/* <ProjectInfoCard /> */}
       {/* <TestomonialsCard /> */}
-      <iframe
-        className='m-2 rounded-lg'
+      {/* <iframe
+        className='spotify-embed rounded-3xl'
         src='https://open.spotify.com/embed/album/0JLN7JryQ2T7lBEYIrSQF1?utm_source=generator&theme=0'
-        width='98%'
-        height='172'
+        width='100%'
+        height='372px'
         allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
         loading='lazy'
-      ></iframe>
-      <FooterContactCard />
-    </>
+      ></iframe> */}
+      {/* <FooterContactCard /> */}
+    </div>
   );
 }
