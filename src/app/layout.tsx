092fd927ee839d175aa732c8e174f10227e1a45a
Anand Sandilya <seanjin17@gmail.com>
2023-12-27 16:00:38 +0100

    feat(base-layout): add site metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/providers/theme-provider';
-import './globals.css';
 import { cn } from '@/lib/utils';
 import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar';
 import { DarkModeToggle } from '@/components/dark-mode-switch';
@@ -14,8 +13,39 @@ import Link from 'next/link';
 const inter = Inter({ subsets: ['greek'], weight: '300' });
 
 export const metadata: Metadata = {
-  title: 'Anand Sandilya',
-  description: 'Some random web engineer on the internet',
+  metadataBase: new URL('https://sandilya.dev'),
+  title: {
+    default: 'Anand Sandilya',
+    template: '%s | Anand Sandilya',
+  },
+  description: 'Full stack engineer, Weekend freelancer',
+  openGraph: {
+    title: 'Anand Sandilya',
+    description: 'Full stack engineer, Weekend freelancer',
+    url: 'https://sandilya.dev',
+    siteName: 'Anand Sandilya',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: 'Anand Sandilya',
+    card: 'summary_large_image',
+  },
+  verification: {
+    // google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
+    // yandex: '14d2e73487fa6c71',
+  },
 };
 
 export default function RootLayout({
