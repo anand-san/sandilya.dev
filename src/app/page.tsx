4c301652cf082c79dfbb6e52b6f023839056c057
Anand Sandilya <seanjin17@gmail.com>
2023-12-29 15:35:52 +0100

    feat(home-page): render  featured project and spotify embed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,24 @@ import ProjectInfoCard from '@/components/project-info-card';
 import FooterContactCard from '@/components/footer-contact-card';
 import TestomonialsCard from '@/components/testomonials-card';
 import './page.css';
+import ProjectInfoContainer from '@/components/projects';
 
 export default function Home() {
   return (
     <div className='base-page m-2'>
       <ProfileInfoCard />
-
-      {/* <ProjectInfoCard /> */}
-      {/* <TestomonialsCard /> */}
-      {/* <iframe
+      <ProjectInfoContainer />
+      <iframe
         className='spotify-embed rounded-3xl'
         src='https://open.spotify.com/embed/album/0JLN7JryQ2T7lBEYIrSQF1?utm_source=generator&theme=0'
         width='100%'
-        height='372px'
+        height='172px'
         allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
         loading='lazy'
-      ></iframe> */}
+      ></iframe>
+      {/* <ProjectInfoCard /> */}
+      {/* <TestomonialsCard /> */}
+
       {/* <FooterContactCard /> */}
     </div>
   );
