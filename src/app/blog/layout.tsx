a913a255b4d8ed1c6cfa6dfa0e7075df50d19e71
Anand Sandilya <seanjin17@gmail.com>
2024-01-01 17:06:00 +0100

    feat: add a blog padding on mobile

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -4,7 +4,7 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='mx-auto mb-16 mt-4 flex w-full max-w-3xl flex-col items-start justify-center sm:mt-8'>
+    <div className='mx-auto mb-16 mt-4 flex w-full max-w-3xl flex-col items-start justify-center p-2 sm:mt-8 sm:p-0'>
       {children}
     </div>
   );
