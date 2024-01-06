bf24c2caf05b10c52310508064b8b619f4f774c1
Anand Sandilya <seanjin17@gmail.com>
2024-01-06 22:00:47 +0100

    fix: blog list layout

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -5,7 +5,18 @@ export default function BlogLayout({
 }) {
   return (
     <div className='mx-auto mb-16 flex w-full max-w-3xl flex-col items-start justify-center'>
-      {children}
+      <section className='w-full pt-6 sm:pt-2'>
+        <div className='flex flex-col'>
+          <div className='flex-1 space-y-4'>
+            <p className='text-center text-lg text-muted-foreground sm:text-xl'>
+              Read my case studies, thoughts on software development, design,
+              and more.
+            </p>
+          </div>
+        </div>
+        <hr className='m-auto my-10 w-24' />
+        {children}
+      </section>
     </div>
   );
 }
--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -2,38 +2,25 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return (
-    <section className='container py-6 lg:py-10'>
-      <div className='flex flex-col'>
-        <div className='flex-1 space-y-4'>
-          <p className='text-lg text-muted-foreground sm:text-xl'>
-            Read my case studies, thoughts on software development, design, and
-            more.
-          </p>
-        </div>
-      </div>
-      <hr className='m-auto my-10 w-24' />
-      <BlogCardSkeleton />
-    </section>
-  );
+  return <BlogCardSkeleton />;
 }
 
-const BlogCardSkeleton = () => {
+export const BlogCardSkeleton = () => {
   return (
-    <div className='flex flex-col sm:flex-row'>
-      <Skeleton className='hidden h-[100px] w-[100px] rounded-[4px] border transition-colors dark:border-neutral-700 sm:block' />
+    <div className='flex h-[150px] w-full flex-col items-center rounded-[4px] bg-stone-100 p-4 py-4 dark:bg-gray-800 sm:flex-row'>
+      <Skeleton className='hidden h-[120px] w-[120px] rounded-[4px] border transition-colors dark:border-neutral-700 sm:block' />
 
-      <div className='flex w-full flex-col justify-center space-y-2 sm:ml-4'>
+      <div className='flex w-5/6 flex-col justify-center sm:ml-4'>
         <span className='text-2xl font-extrabold'>
           <Skeleton className='h-[32px] w-3/6' />
         </span>
-        <span className='text-muted-foreground'>
+        <span className='mt-4 text-muted-foreground'>
           <Skeleton className='h-[24px] w-full' />
         </span>
-        <div className='flex flex-row items-center gap-2'>
-          <Skeleton className='h-[16px] w-[60px]' />
-          <Skeleton className='h-[16px] w-[40px]' />
-          <Skeleton className='h-[16px] w-[60px]' />
+        <div className='mt-4 flex flex-row items-center gap-2'>
+          <Skeleton className='h-[20px] w-[60px]' />
+          <Skeleton className='h-[20px] w-[40px]' />
+          <Skeleton className='h-[20px] w-[60px]' />
         </div>
       </div>
     </div>
