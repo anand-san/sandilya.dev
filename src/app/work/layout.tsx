9a077710548ec5d3efaafe9dcc3310c4c051e046
Anand Sandilya <seanjin17@gmail.com>
2024-01-06 22:03:31 +0100

    fix: move fixed content to layout

--- src/app/work/layout.tsx
+++ src/app/work/layout.tsx
@@ -5,7 +5,17 @@ export default function BlogLayout({
 }) {
   return (
     <div className='mx-auto mb-16 flex w-full max-w-3xl flex-col items-start justify-center'>
-      {children}
+      <section className='pt-6 sm:pt-2'>
+        <div className='flex flex-col'>
+          <div className='flex-1 space-y-4'>
+            <p className='text-center text-lg text-muted-foreground sm:text-xl'>
+              A summary of my work and contributions.
+            </p>
+          </div>
+        </div>
+        <hr className='m-auto my-10 w-24' />
+        {children}
+      </section>
     </div>
   );
 }
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -8,21 +8,10 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <section className='pt-6 sm:pt-2'>
-      <div className='flex flex-col'>
-        <div className='flex-1 space-y-4'>
-          <p className='text-lg text-muted-foreground sm:text-xl'>
-            A summary of my work and contributions.
-          </p>
-        </div>
-      </div>
-      <hr className='m-auto my-10 w-24' />
-
-      <div className='space-y-4'>
-        {ALL_PROJECTS.sort(sortProjectsByOrder).map((project) => (
-          <ProjectCard project={project} key={project.order} />
-        ))}
-      </div>
-    </section>
+    <div className='space-y-4'>
+      {ALL_PROJECTS.sort(sortProjectsByOrder).map((project) => (
+        <ProjectCard project={project} key={project.order} />
+      ))}
+    </div>
   );
 }
